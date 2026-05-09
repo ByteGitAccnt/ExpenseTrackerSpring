3ae1e36567bdce84e7090369597f8a51b429d993
Shahul <game2018785@gmail.com>
2026-05-10 00:30:53 +0530

    minor changes for jwt , jwt error global exception handling

---
+++ src/main/java/com/myApp/ExpenseTracker/Exeception/AuthException.java
@@ -0,0 +1,16 @@
+package com.myApp.ExpenseTracker.Exeception;
+
+import org.springframework.http.HttpStatus;
+
+public abstract class AuthException extends RuntimeException {
+    private final HttpStatus status;
+
+    protected AuthException(String message, HttpStatus status) {
+        super(message);
+        this.status = status;
+    }
+
+    public HttpStatus getStatus() {
+        return status;
+    }
+}
