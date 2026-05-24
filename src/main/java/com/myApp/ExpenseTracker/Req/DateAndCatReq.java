11d3977a3c281bd253243fa155a7272c107f4920
Shahul <game2018785@gmail.com>
2026-05-25 00:14:51 +0530

    list issue solved

--- src/main/java/com/myApp/ExpenseTracker/Req/DateAndCatReq.java
+++ src/main/java/com/myApp/ExpenseTracker/Req/DateAndCatReq.java
@@ -1,5 +1,7 @@
 package com.myApp.ExpenseTracker.Req;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import lombok.Builder;
@@ -16,8 +18,12 @@ public class DateAndCatReq {
     private LocalDate endDate;
     @Positive
     private Long catid;
+    @Min(value = 0, message = "page must be at least 0")
+    @JsonProperty(defaultValue = "1")
     @Builder.Default
     private int page = 0;
+    @Min(value = 1, message = "size must be at least 1")
+    @JsonProperty(defaultValue = "10")
     @Builder.Default
     private int size = 10;
 }
--- src/main/java/com/myApp/ExpenseTracker/Req/DateReq.java
+++ src/main/java/com/myApp/ExpenseTracker/Req/DateReq.java
@@ -1,5 +1,7 @@
 package com.myApp.ExpenseTracker.Req;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Getter;
@@ -13,8 +15,12 @@ public class DateReq {
     private LocalDate startDate;
     @NotNull
     private LocalDate endDate;
+    @Min(value = 0, message = "page must be at least 0")
+    @JsonProperty(defaultValue = "1")
     @Builder.Default
     private int page = 0;
+    @Min(value = 1, message = "size must be at least 1")
+    @JsonProperty(defaultValue = "10")
     @Builder.Default
     private int size = 10;
 }
