5a500cb8c284886c49116180918a4e76f5266d49
Shahul <game2018785@gmail.com>
2026-06-05 22:46:40 +0530

    for Deployment

--- src/main/java/com/myApp/ExpenseTracker/Controller/AppController.java
+++ src/main/java/com/myApp/ExpenseTracker/Controller/AppController.java
@@ -1,45 +1,22 @@
 package com.myApp.ExpenseTracker.Controller;
 
 import com.myApp.ExpenseTracker.Dto.AppInfoResponse;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.UrlResource;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 @RestController
 @RequestMapping("/api/app")
 public class AppController {
     @GetMapping("/info")
     public ResponseEntity <AppInfoResponse> getAppInfo(){
+        //https://expense-tracker-dtfp.onrender.com/apk/app-release.apk direct download by this link
         return ResponseEntity.ok().body(new AppInfoResponse(
                 "1.0.0",
                 "1.0.0" ,
                 false,
-                "src/main/resources/static/apk/.apk"
+                "https://expense-tracker-dtfp.onrender.com/apk/app-release.apk"
         ));
-    }
-    @GetMapping("/latest")
-    public ResponseEntity<Resource> downloadLatestVersion() throws IOException {
-        Path path = Paths.get(
-                "src/main/resources/static/apk/app-release.apk"
-        );
-        Resource resource = new UrlResource(path.toUri());
-        return ResponseEntity.ok()
-                .contentType(
-                        MediaType.parseMediaType(
-                                "application/vnd.android.package-archive"
-                        )
-                ).header(
-                        HttpHeaders.CONTENT_DISPOSITION,
-                        "attachment; filename=\"app-release.apk\""
-                ).body(resource);
     }
 }
--- src/main/java/com/myApp/ExpenseTracker/Model/CustomUserDetails.java
+++ src/main/java/com/myApp/ExpenseTracker/Model/CustomUserDetails.java
@@ -1,6 +1,5 @@
 package com.myApp.ExpenseTracker.Model;
 
-import lombok.Getter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
